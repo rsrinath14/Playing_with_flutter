9695a611c60549a272a7823314e1875dd792f338
R_Srinath <srinath1412001@gmail.com>
2020-07-29 19:15:01 +0530

    TextFields added

--- responding_to_user_inputs/lib/main.dart
+++ responding_to_user_inputs/lib/main.dart
@@ -26,6 +26,7 @@ class MyWidget extends StatefulWidget {
 
 class _MyWidgetState extends State<MyWidget> {
   String textString = 'Hello World!';
+  String textBox = 'Type Something';
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +47,22 @@ class _MyWidgetState extends State<MyWidget> {
               _doSomething();
             },
           ),
+          Container(
+            height: 50,
+          ),
+          Text(
+            textBox,
+            style: TextStyle(fontSize: 40),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              style: TextStyle(fontSize: 20),
+              onChanged: (text) {
+                _textchange(text);
+              },
+            ),
+          )
         ],
       ),
     );
@@ -56,6 +73,12 @@ class _MyWidgetState extends State<MyWidget> {
       textString == 'Hello Flutter!'
           ? textString = 'Hello World!'
           : textString = 'Hello Flutter!';
+    });
+  }
+
+  void _textchange(String text) {
+    setState(() {
+      textBox = text;
     });
   }
 }
